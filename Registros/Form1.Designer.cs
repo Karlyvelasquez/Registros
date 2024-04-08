2a4350824e4cea02e80879844f0d27b7d70f9443
SALA 404 <SALA 404@U-38215>
2024-04-08 12:57:42 -0500

    Agregar archivos de proyecto.

---
+++ Registros/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace Registros
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtLinea = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.dtgDatos = new System.Windows.Forms.DataGridView();
+            this.btnPorLinea = new System.Windows.Forms.Button();
+            this.btnCSV = new System.Windows.Forms.Button();
+            this.Col1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLinea
+            // 
+            this.txtLinea.Location = new System.Drawing.Point(96, 60);
+            this.txtLinea.Multiline = true;
+            this.txtLinea.Name = "txtLinea";
+            this.txtLinea.Size = new System.Drawing.Size(261, 245);
+            this.txtLinea.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dtgDatos
+            // 
+            this.dtgDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgDatos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Col1,
+            this.Col2,
+            this.Col3});
+            this.dtgDatos.Location = new System.Drawing.Point(381, 60);
+            this.dtgDatos.Name = "dtgDatos";
+            this.dtgDatos.Size = new System.Drawing.Size(348, 245);
+            this.dtgDatos.TabIndex = 2;
+            // 
+            // btnPorLinea
+            // 
+            this.btnPorLinea.Location = new System.Drawing.Point(247, 347);
+            this.btnPorLinea.Name = "btnPorLinea";
+            this.btnPorLinea.Size = new System.Drawing.Size(107, 42);
+            this.btnPorLinea.TabIndex = 3;
+            this.btnPorLinea.Text = "Linea a Linea";
+            this.btnPorLinea.UseVisualStyleBackColor = true;
+            this.btnPorLinea.Click += new System.EventHandler(this.btnPorLinea_Click);
+            // 
+            // btnCSV
+            // 
+            this.btnCSV.Location = new System.Drawing.Point(395, 349);
+            this.btnCSV.Name = "btnCSV";
+            this.btnCSV.Size = new System.Drawing.Size(95, 40);
+            this.btnCSV.TabIndex = 4;
+            this.btnCSV.Text = "CSV";
+            this.btnCSV.UseVisualStyleBackColor = true;
+            this.btnCSV.Click += new System.EventHandler(this.btnCSV_Click);
+            // 
+            // Col1
+            // 
+            this.Col1.HeaderText = "Column1";
+            this.Col1.Name = "Col1";
+            // 
+            // Col2
+            // 
+            this.Col2.HeaderText = "Column2";
+            this.Col2.Name = "Col2";
+            // 
+            // Col3
+            // 
+            this.Col3.HeaderText = "Column3";
+            this.Col3.Name = "Col3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCSV);
+            this.Controls.Add(this.btnPorLinea);
+            this.Controls.Add(this.dtgDatos);
+            this.Controls.Add(this.txtLinea);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtgDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLinea;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.DataGridView dtgDatos;
+        private System.Windows.Forms.Button btnPorLinea;
+        private System.Windows.Forms.Button btnCSV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col3;
+    }
+}
+
---
+++ Registros/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Registros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private Stream myStream;
+        int counter  = 0;
+        string line;
+
+        private void btnPorLinea_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Application.StartupPath;
+            openFileDialog.Filter = "Archivos (+.txt) | +.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+
+                try
+                {
+                    if ((myStream = openFileDialog.OpenFile()) != null) 
+                    {
+                        using (myStream) 
+                        {
+                            System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog.FileName);  
+                            
+                            while ((line = file.ReadLine()) != null) 
+                            {
+                                txtLinea.Text = txtLinea.Text + line;
+                                counter++;
+
+                            }
+                            file.Close();
+                        }                   
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+        }
+
+        private void btnCSV_Click(object sender, EventArgs e)
+        {
+            DataGridViewTextBoxColumn col1 = new DataGridViewTextBoxColumn();
+            col1.HeaderText = "ID";
+            col1.Width = 200;
+            col1.ReadOnly = true;
+            dtgDatos.Columns.Add(col1);
+
+            DataGridViewTextBoxColumn col2 = new DataGridViewTextBoxColumn();
+            col2.HeaderText = "Nombre";
+            col2.Width = 200;
+            col2.ReadOnly = true;
+            dtgDatos.Columns.Add(col2);
+
+            DataGridViewTextBoxColumn col3 = new DataGridViewTextBoxColumn();
+            col3.HeaderText = "Apellido";
+            col3.Width = 200;
+            col3.ReadOnly = true;
+            dtgDatos.Columns.Add(col3);
+
+            char delimitador = ';';
+            string[] valores;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Application.StartupPath;
+            openFileDialog.Filter = "Archivos (*.csv) | *.csv";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    if ((myStream = openFileDialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            System.IO.StreamReader file = new StreamReader(openFileDialog.FileName);
+
+                            while ((line = file.ReadLine ()) != null) 
+                            {
+
+                                if (counter >= 1)
+                                {
+                                    valores = line.Split(delimitador);
+                                    dtgDatos.Rows.Add(valores.ToArray());
+                                    counter++;
+                                }
+                                else
+                                {
+                                    counter++;
+
+                                }
+                                
+                                }  
+                            file.Close();
+
+
+                            }
+                        
+
+                        }
+                    }
+
+                
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message);
+                }
+
+            }
+
+        }
+    }
+}
